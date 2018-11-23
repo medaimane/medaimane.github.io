ddc08e0f890f24b1edf1a17b58b1199984a94202
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2018-11-23 16:22:26 +0000

    feat: Turn js code to es6 syntax (arrow func, async await)

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@ var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
   '/',
   '/favicon.ico',
-
   '/assets/css/images/ie/footer.png',
   '/assets/css/images/ie/footer.svg',
   '/assets/css/images/bg.jpg',
@@ -19,65 +18,51 @@ var urlsToCache = [
   '/assets/fonts/fontawesome-webfont.woff2',
   '/assets/images/icons8-source-code-48.ico',
   '/assets/images/icons8-source-code-48.png',
-  '/assets/sass/libs/_breakpoints.scss',
-  '/assets/sass/libs/_functions.scss',
-  '/assets/sass/libs/_mixins.scss',
-  '/assets/sass/libs/_vars.scss',
-  '/assets/sass/libs/_vendor.scss',
-  '/assets/sass/main.scss',
-  '/assets/sass/noscript.scss',
-
-  '/js/index.js'
+  '/js/index.js',
 ];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', async event => {
   // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(function (cache) {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    })
-  );
+  event.waitUntil(() => {
+    const cache = await caches.open(CACHE_NAME);
+    console.log('Opened cache');
+    return cache.addAll(urlsToCache);
+  });
 });
 
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-    .then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
+self.addEventListener('fetch', async event => {
+  event.respondWith(() => {
+    const response = await caches.match(event.request);
 
-      // IMPORTANT: Clone the request. A request is a stream and
-      // can only be consumed once. Since we are consuming this
-      // once by cache and once by the browser for fetch, we need
-      // to clone the response.
-      var fetchRequest = event.request.clone();
+    // Cache hit - return response
+    if (response) {
+      return response;
+    }
 
-      return fetch(fetchRequest).then(
-        function (response) {
-          // Check if we received a valid response
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
+    // IMPORTANT: Clone the request. A request is a stream and
+    // can only be consumed once. Since we are consuming this
+    // once by cache and once by the browser for fetch, we need
+    // to clone the response.
+    var fetchRequest = event.request.clone();
 
-          // IMPORTANT: Clone the response. A response is a stream
-          // and because we want the browser to consume the response
-          // as well as the cache consuming the response, we need
-          // to clone it so we have two streams.
-          var responseToCache = response.clone();
+    response = await fetch(fetchRequest);
 
-          caches.open(CACHE_NAME)
-            .then(function (cache) {
-              cache.put(event.request, responseToCache);
-            });
+    // Check if we received a valid response
+    if (!response || response.status !== 200 || response.type !== 'basic') {
+      return response;
+    }
 
-          return response;
-        }
-      );
-    })
-  );
+    // IMPORTANT: Clone the response. A response is a stream
+    // and because we want the browser to consume the response
+    // as well as the cache consuming the response, we need
+    // to clone it so we have two streams.
+    var responseToCache = response.clone();
+
+    const cache = caches.open(CACHE_NAME);
+    
+    cache.put(event.request, responseToCache);
+
+    return response;
+  });
 });
