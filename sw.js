075665fcc60c54dc87b02c712c5bc9017ba6c149
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2018-11-23 17:02:35 +0000

    fix: define response variable with let

--- sw.js
+++ sw.js
@@ -33,31 +33,21 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith( async () => {
-    const response = await caches.match(event.request);
+    let response = await caches.match(event.request);
 
-    // Cache hit - return response
     if (response) {
       return response;
     }
 
-    // IMPORTANT: Clone the request. A request is a stream and
-    // can only be consumed once. Since we are consuming this
-    // once by cache and once by the browser for fetch, we need
-    // to clone the response.
-    var fetchRequest = event.request.clone();
+    const fetchRequest = event.request.clone();
 
     response = await fetch(fetchRequest);
 
-    // Check if we received a valid response
     if (!response || response.status !== 200 || response.type !== 'basic') {
       return response;
     }
 
-    // IMPORTANT: Clone the response. A response is a stream
-    // and because we want the browser to consume the response
-    // as well as the cache consuming the response, we need
-    // to clone it so we have two streams.
-    var responseToCache = response.clone();
+    const responseToCache = response.clone();
 
     const cache = await caches.open(CACHE_NAME);
 
